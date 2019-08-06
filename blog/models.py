a867a052c014716ae83626999e20066b00355b7a
eungbean <eungbean@u.sogang.ac.kr>
2019-08-06 20:29:37 +0900

    image upload feature added

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to="blog/%Y/%m/%d", blank=True)
+
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=False) #회원이 탈퇴하면 글도 같이 지울건지 (=True)
 
